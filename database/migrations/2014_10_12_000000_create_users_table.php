fc1b9895e01903f60f628f29cef16415d75734c5
Ángel Quiroz <guirudj007@gmailc.om>
2021-08-09 01:42:14 -0500

    Password Default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -19,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default(Hash::make('password'));
             $table->string('google_id')->nullable();
             $table->string('remember_token', 100)->nullable();
             $table->foreignId('current_team_id')->nullable();
