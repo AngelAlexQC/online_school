85cfc2c85385c2225183497e515312e4d1ffe8e5
Ángel Quiroz <guirudj007@gmailc.om>
2021-08-14 20:57:13 -0500

    Upload Files

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\CourseClassController;
 use App\Http\Controllers\GoogleController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,35 @@ use App\Http\Controllers\GoogleController;
 
 Route::get('/', function () {
     return redirect('dashboard');
+});
+// upload file
+Route::post('/upload', function (Request $request) {
+    // Validate the file
+    $file = $request->file('file');
+    if (!$file) {
+        return response()->json(['error' => 'File is required'], 422);
+    }
+    // Get multiple uploaded files
+    $files = $request->file('file');
+    // Get the name of the uploaded file
+    $name = $request->file('file')->getClientOriginalName();
+    // Get the mime type of the uploaded file
+    $type = $request->file('file')->getClientMimeType();
+    // Get the path of the uploaded file
+    $path = $request->file('file')->getRealPath();
+    // Get the extension of the uploaded file
+    $ext = pathinfo($name, PATHINFO_EXTENSION);
+    // Get the name of the uploaded file without extension
+    $filename = pathinfo($name, PATHINFO_FILENAME);
+    // Get the name of the uploaded file without extension and with random string
+    $filename = md5(uniqid(mt_rand(), true)) . '.' . $ext;
+    // Get the path of the uploaded file without extension and with random string
+    $path = public_path('uploads/');
+    // Save the uploaded file
+    $request->file('file')->move($path, $filename);
+    // Return the path of the uploaded file
+    $url = url('/uploads/' . $filename);
+    return $url;
 });
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
 Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
